965b92238fd23bee6fac21e73670dfbd4a130ae1
kwesilarry1 <appiahbis999@gmail.com>
2025-06-16 10:16:02 +0000

    update on the config file

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,16 @@
 <?php
-$host = 'localhost';
-$db = 'car_rental';
-$user = 'root';
-$pass = '';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
+$host = 'localhost';
+$db = 'car_rental';  
+$user = 'root';
+$pass = '';       
+
+// Create MySQL connection
 $conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
